a631174404ff1afae25e20bed8fb382c29c5029e
Mdealba <M_dealba@yahoo.com>
2018-05-02 19:30:56 -0700

    Updated and Finished Game Questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,45 @@
 var questions = [{
-    question: "Who loves orange soda?",
-    answers: ["Bill Clinton", "Yo Mama", "Kel", "Tom from Myspace"],
-    correctAnswer: "Kel"
-},{
     questions: "What is Pandas real name",
     answers: ["Pedro Sandoval", "Pablo Sandoval", "Paco Taco", "Jose Sanchez"],
     correctAnswer: "Pablo Sandoval"
 },{
+    questions: "How old is Panda, as of May 2, 2018?",
+    answers: ["31", "29", "33", "38"],
+    correctAnswer: "31"
+},{
+    questions: "Where was Panda born?",
+    answers: ["Colombia", "Venezula", "Puerto Rico", "Brazil"],
+    correctAnswer: "Venezuela"
+},{
     questions: "What number is Panda?",
     answers: ["23", "8", "42", "48"],
     correctAnswer: "48"
-},
+},{
+    questions: "What position does Panda Play?",
+    answers: ["RF", "CF", "3B", "2B"],
+    correctAnswer: "3B"
+
+},{
+    questions: "What other team did Panda Play for?",
+    answers: ["Red Socks", "White Socks", "Dodgers", "Tigers"],
+    correctAnswer: "Red Socks"
+},{
+    questions: "How may home runs has Panda hit, as of May 2, 2018?",
+    answers: ["126", "213", "153", "137"],
+    correctAnswer: "126"
+},{
+    questions: "What World Series did Panda hit 3 home runs in game 1?",
+    answers: ["2010", "2014", "2012", "2017"],
+    correctAnswer: "2012"
+},{
+    questions: "How many World Champioships has Panda Won?",
+    answers: ["4", "1", "6", "3"],
+    correctAnswer: "3"
+},{
+    questions: "Who is Panda playing for, as of May 2, 2018?",
+    answers: ["Dogers", "Giants", "Red Socks", "Tigers"],
+    correctAnswer: "Giants"
+}
 ]
 
 // function to set up my game (onclick)
